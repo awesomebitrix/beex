9c9bffe0aca444230a28726a7870e235e15056b9
Konstantin <pushin.konstantin@gmail.com>
2012-08-09 12:28:04 +0400

    Кеш и несколько базовых классов

---
+++ pushin.dm/classes/general/base/dmArray.class.php
@@ -0,0 +1,31 @@
+<?php
+class dmArray {
+
+    public static function get($array, $key, $default = null) {
+    
+        return isset($array[$key])? $array[$key] : $default;
+    
+    }
+    
+    public static function first($array) {
+    
+        return array_shift($array);
+    
+    }
+
+    public static function lasr($array) {
+    
+        return array_pop($array);
+    
+    }
+    
+    public static function fetch($array) {
+    
+        if (is_null($array)) $array = array();
+        if (!is_array($array)) $array = array($array);
+    
+        return $array;
+    
+    }    
+    
+}
---
+++ pushin.dm/classes/general/cache/dmCache.class.php
@@ -0,0 +1,55 @@
+<?php
+class dmCache extends dmConfigurable {
+    
+    public function __construct($options = array()) {
+
+        $this->configure($options);
+    
+    }
+    
+    public function getDefaultOptions() {
+    
+        return array(
+            'rootPath' => 'djem_cache',
+            'lifetime' => 864000,
+        );
+    
+    }
+    
+    public function get($key) {
+            
+        $cache = new CPHPCache();
+        if (!$cache->InitCache(null, $key, $this->getPath())) return null;
+        
+        return $cache->GetVars();
+    
+    }
+    
+    public function has($key) {
+    
+        $cache = new CPHPCache();
+        
+        return $cache->InitCache(null, $key, $this->getPath());
+        
+    }
+    
+    public function set($key, $value, $lifetime = null) {
+    
+        if (is_null($lifetime)) $lifetime = $this->getOption('lifetime');
+        
+        $cache = new CPHPCache();
+        $cache->StartDataCache($lifetime, $key, $this->getPath());
+        $cache->EndDataCache($value);
+        
+    }
+    
+    public function getPath() {
+    
+        $rootPath = $this->getOption('rootPath');
+        $path = $this->getOption('path');
+    
+        return $path? $rootPath.$path : null;
+    
+    }
+
+}
