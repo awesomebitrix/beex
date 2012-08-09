32a7c246df95f97285c2cfe96a8d379a1a54af12
Konstantin <pushin.konstantin@gmail.com>
2012-08-09 11:31:16 +0400

    Начало
    Каркас модуля

---
+++ pushin.dm/install/index.php
@@ -0,0 +1,31 @@
+<?php
+// include_once $_SERVER['DOCUMENT_ROOT'].'/bitrix/admin/modules/picom_linked_properties/prolog.php';
+
+class pushin_dm extends CModule
+{
+    public $MODULE_ID = 'pushin.dm';
+    public $MODULE_NAME = 'Привязка свойств инфоблока к разделам';
+    public $MODULE_DESCRIPTION = 'Дает возможность задать для товаров разных категорий разные наборы свойств';
+
+    public $MODULE_VERSION = '1.0';
+    public $MODULE_VERSION_DATE = '2012-05-17';
+    
+	public function __construct() 
+    {
+    
+		$arModuleVersion = array();
+
+		include(dirname(__FILE__).'/version.php');
+
+		$this->MODULE_VERSION = $arModuleVersion["VERSION"];
+		$this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
+
+		$this->MODULE_NAME = 'Djem';
+		$this->MODULE_DESCRIPTION = '';
+		$this->PARTNER_NAME = 'pushin';
+		$this->PARTNER_URI = 'http://pushin.pro';
+	}
+    
+    
+    
+}
---
+++ pushin.dm/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+    'VERSION' => '1.0',
+    'VERSION_DATE' => '2010-06-03 17:00:00',
+);
+?>
