a8ecd576227ae0c3ed0f296f9c145116d29e1cc5
Konstantin Pushin <pushin.konstantin@gmail.com>
2012-08-18 12:23:51 +0400

    Ресайз изображений

--- classes/general/core/autoloader/dmAutoloader.class.php
+++ classes/general/core/autoloader/dmAutoloader.class.php
@@ -4,78 +4,78 @@ class dmAutoloader {
     protected $libs = array();
 
     protected $paths = array();
-    
+
     public function init() {
-    
+
         $config = dmCore::getService('config');
-    
+
         $this->addLib($this->getModuleLibPath());
         $events = GetModuleEvents($config->get('dmModuleId'), 'OnBeforeDmAutoloaderRegister');
 
-        while($event = $events->Fetch()) ExecuteModuleEvent($event, $autoloader);    
-    
+        while($event = $events->Fetch()) ExecuteModuleEvent($event, $this);
+
         return $this;
-    
+
     }
-    
+
     protected function getModuleLibPath() {
-    
+
         $config = dmCore::getService('config');
-        
+
         $path = $config->get('paths/module');
         if (!$path) throw new Exception('Module path is not specified');
-        
+
         return $path.'/classes/general/lib';
-    
+
     }
-        
+
     public function addLib($path) {
 
         if (!is_dir($path)) return;
-        
+
         $this->libs[] = new dmAutoloaderLib($path);
-    
+
     }
-    
+
     public function register() {
-    
+
         CModule::AddAutoloadClasses('', $this->getPaths());
-    
-    }    
-    
-    public function getPaths() {
-    
-        if (!$this->paths) $this->loadPaths();
-    
-        return $this->paths;
-    
+
     }
-    
+
+    public function getPaths() {
+
+        if (!$this->paths) $this->loadPaths();
+
+        return $this->paths;
+
+    }
+
     public function loadPaths() {
-    
+
         $cache = new dmAutoloaderCache();
-        
+
         $this->paths = $cache->get('paths');
         $this->paths = array();
-        
+
         if (!$this->paths) {
-        
+
             $this->paths = $this->findPaths();
-        
+
             $cache->set('paths', $this->paths);
-            
+
         }
-            
+
     }
-    
+
     protected function findPaths() {
-  
+
         $classes = array();
-    
+
         foreach($this->libs as $lib) $classes = array_merge($classes, $lib->findClasses());
-        
+
         return $classes;
-    
+
     }
 
 }
---
+++ classes/general/lib/image/dmImage.class.php
@@ -0,0 +1,123 @@
+<?php
+class dmImage {
+
+    protected $fileId = null;
+
+    protected $attributes = null;
+
+    protected $originalData = null;
+
+    public static function create($fileId, $loadOriginalData = false) {
+
+        $image = new self();
+        $image->setFileId($fileId, $loadOriginalData);
+        return $image;
+
+    }
+
+    public function __toString() {
+
+        return $this->render();
+
+    }
+
+    public function getOriginalWidth() {
+
+        return dmArray::get($this->originalData, 'WIDTH');
+
+    }
+
+    public function getOriginalHeight() {
+
+        return dmArray::get($this->originalData, 'HEIGHT');
+
+    }
+
+    public function getOriginalSrc() {
+
+        return dmArray::get($this->originalData, 'SRC');
+
+    }
+
+    public function getOriginalData($name) {
+
+        return dmArray::get($this->originalData, $name);
+
+    }
+
+    public function setFileId($fileId, $loadOriginalData = false) {
+
+        if (is_array($fileId)) {
+
+            $this->originalData = $fileId;
+
+            $fileId = dmArray::get($this->originalData, 'ID');
+
+        }
+        elseif(loadOriginalData) {
+
+            $this->originalData = CFile::GetFileArray($fileId);
+
+        }
+
+        $this->fileId = $fileId;
+
+    }
+
+    public function setAttributes($attributes) {
+
+        $this->attributes = $attributes;
+
+        return $this;
+
+    }
+
+    public function size($width, $height = 999999) {
+
+        $this->width($width);
+        $this->height($height);
+
+        return $this;
+
+    }
+
+    public function width($width) {
+
+        $this->width = $width;
+
+        return $this;
+
+    }
+
+    public function height($height) {
+
+        $this->height = $height;
+
+        return $this;
+
+    }
+
+    public function render() {
+        $html = '';
+        if ($this->fileId) {
+            $arFileTmp = CFile::ResizeImageGet(
+                $this->fileId,
+                array("width" => $this->width, "height" => $this->height),
+                BX_RESIZE_IMAGE_PROPORTIONAL,
+                true,
+                array()
+            );
+            if ($arFileTmp["width"] && $arFileTmp["height"] && $arFileTmp["src"]) {
+                $html = '
+                    <img
+                        width="'.$arFileTmp["width"].'"
+                        height="'.$arFileTmp["height"].'"
+                        src="'.$arFileTmp["src"].'"
+                        '.$this->attributes.'
+                    >
+                ';
+            }
+        }
+        return $html;
+    }
+}
