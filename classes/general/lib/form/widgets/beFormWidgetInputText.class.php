0acba59ad9595316114116b501303f92af726968
konstantin <pushin.konstantin@gmail.com>
2013-02-20 10:46:02 +0400

    Fix: beFormWidgetInputText was rendered without type attribute

--- classes/general/lib/form/widgets/beFormWidgetInputText.class.php
+++ classes/general/lib/form/widgets/beFormWidgetInputText.class.php
@@ -1,4 +1,10 @@
 <?php
 class beFormWidgetInputText extends beFormWidget {
 
+    protected function renderFieldWithAttributes($attributes = array()) {
+        $attributes['type'] = 'text';
+        return parent::renderFieldWithAttributes($attributes);
+    }
+
+
 }
